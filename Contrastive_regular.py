deef73c86875584ce3d0c4d7d98c031161989656
Tingyi <tiwanyan@gmail.com>
2020-12-09 19:23:46 -0500

    correction

--- Contrastive_regular.py
+++ Contrastive_regular.py
@@ -21,8 +21,8 @@ class con_regular():
         # self.hetro_model = hetro_model
         self.train_data_whole = self.data_process.train_patient_whole
         self.test_data_whole = self.data_process.test_patient_whole
-        #self.train_data = self.train_data_whole[0]
-        #self.test_data = self.test_data_whole[0]
+        self.train_data = self.train_data_whole[0]
+        self.test_data = self.test_data_whole[0]
         self.softmax_weight_threshold = 0.1
         #self.length_train = len(self.train_data)
         #self.length_test = len(self.test_data)
