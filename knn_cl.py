8f76adb790815dc86acb7440f51196960c53f21e
Tingyi <tiwanyan@gmail.com>
2021-01-20 02:32:48 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -978,9 +978,9 @@ class knn_cl():
         for j in range(self.epoch):
             print('epoch')
             print(j)
-            self.construct_knn_graph()
+            #self.construct_knn_graph()
             for i in range(iteration):
-                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train(
+                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train_origin(
                     self.batch_size, i * self.batch_size, self.train_data)
 
                 self.err_ = self.sess.run([self.negative_sum_contrast, self.train_step_neg],
@@ -1009,7 +1009,7 @@ class knn_cl():
             print('epoch')
             print(j)
             for i in range(iteration):
-                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train(
+                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train_origin(
                     self.batch_size, i * self.batch_size, self.train_data)
 
                 self.err_ = self.sess.run([self.cross_entropy, self.train_step_ce],
@@ -1141,7 +1141,7 @@ class knn_cl():
 
 
         self.config_model()
-        for i in range(3):
+        for i in range(2):
             self.sess = tf.InteractiveSession()
             tf.global_variables_initializer().run()
             tf.local_variables_initializer().run()
