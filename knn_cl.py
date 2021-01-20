cf58e97a7f988297f7cfd9b189f95b6c0c0da4c2
Tingyi <tiwanyan@gmail.com>
2021-01-20 04:32:36 -0500

    correciton

--- knn_cl.py
+++ knn_cl.py
@@ -978,9 +978,9 @@ class knn_cl():
         for j in range(self.epoch):
             print('epoch')
             print(j)
-            #self.construct_knn_graph()
+            self.construct_knn_graph()
             for i in range(iteration):
-                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train_origin(
+                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train(
                     self.batch_size, i * self.batch_size, self.train_data)
                 self.err_ = self.sess.run([self.negative_sum_contrast, self.train_step_neg],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
