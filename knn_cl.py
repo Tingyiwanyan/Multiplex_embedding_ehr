2d5b55b3c47e9a3783b9eff2c3effd4761a947e2
Tingyi <tiwanyan@gmail.com>
2021-01-20 03:57:58 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -1045,6 +1045,14 @@ class knn_cl():
                                                                          # self.input_x_com: self.test_com,
                                                                          self.init_hiddenstate: init_hidden_state,
                                                                          self.input_icu_intubation:self.one_batch_icu_intubation})
+
+        self.out_test_patient = self.sess.run(self.Dense_patient, feed_dict={self.input_x_vital: self.test_data_batch_vital,
+                                                                  self.input_x_lab: self.test_one_batch_lab,
+                                                                  self.input_x_demo: self.test_one_batch_demo,
+                                                                  # self.input_x_com: self.test_com,
+                                                                  self.init_hiddenstate: init_hidden_state,
+                                                                  self.input_icu_intubation: self.one_batch_icu_intubation})[:,
+                            0, :]
         """
         self.test_att_score = self.sess.run([self.score_attention,self.input_importance,self.input_x],feed_dict={self.input_x_vital: self.test_data_batch_vital,
                                                                          self.input_x_lab: self.test_one_batch_lab,
