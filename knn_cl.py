7e8bbaa185cb2303d091a82e8d3e8a042c082c40
Tingyi <tiwanyan@gmail.com>
2021-01-27 04:52:55 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -38,7 +38,7 @@ class knn_cl():
         self.latent_dim_att = 100
         self.latent_dim_demo = 50
         self.epoch = 2
-        self.epoch_representation = 4
+        self.epoch_representation = 1
         self.item_size = len(list(kg.dic_vital.keys()))
         self.demo_size = len(list(kg.dic_race.keys()))
         self.lab_size = len(list(kg.dic_lab.keys()))
@@ -599,7 +599,7 @@ class knn_cl():
         self.focal_loss = tf.reduce_mean(self.focal_loss_)
         self.train_step_fl = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.focal_loss)
         self.train_step_combine_fl = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(
-            0.0 * self.focal_loss + 1.0 * self.negative_sum_contrast)
+            0.8 * self.focal_loss + 0.2 * self.negative_sum_contrast)
         self.sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
@@ -1154,7 +1154,7 @@ class knn_cl():
             print(j)
             #self.construct_knn_graph()
             for i in range(iteration):
-                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train_origin(
+                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train(
                     self.batch_size, i * self.batch_size, self.train_data)
                 self.err_ = self.sess.run([self.negative_sum_contrast, self.train_step_neg],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
@@ -1343,9 +1343,9 @@ class knn_cl():
             tf.local_variables_initializer().run()
             self.train_data = self.train_data_whole[i]
             self.test_data = self.test_data_whole[i]
+            self.construct_knn_graph_attribute()
             print("im here in train representation")
             self.train_representation()
-            #self.construct_knn_graph_attribute()
             print("im here in train")
             self.train()
             self.test(self.test_data)
