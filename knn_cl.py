00011e9878ae666b9c89aa32cae2c7b51a856d28
Tingyi <tiwanyan@gmail.com>
2021-01-27 09:39:45 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -1183,7 +1183,7 @@ class knn_cl():
             print(j)
             #self.construct_knn_graph()
             for i in range(iteration):
-                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train_origin(
+                self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train(
                     self.batch_size, i * self.batch_size, self.train_data)
 
                 self.err_ = self.sess.run([self.focal_loss, self.train_step_combine_fl],
