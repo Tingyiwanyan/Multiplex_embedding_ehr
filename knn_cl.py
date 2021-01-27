2b049d262673f6c1b1c05f046ed3b4d54858ad22
Tingyi <tiwanyan@gmail.com>
2021-01-27 03:46:07 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -584,7 +584,7 @@ class knn_cl():
         bce = tf.keras.losses.BinaryCrossentropy()
         self.cross_entropy = bce(self.logit_sig, self.input_y_logit)
         self.train_step_ce = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.cross_entropy)
-        self.train_step_combine_ce = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(0.8*self.cross_entropy+0.2*self.negative_sum_contrast)
+        self.train_step_combine_ce = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(0.6*self.cross_entropy+0.4*self.negative_sum_contrast)
         """
         focal loss
         """
@@ -1186,7 +1186,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train_origin(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.focal_loss, self.train_step_fl],
+                self.err_ = self.sess.run([self.focal_loss, self.train_step_combine_fl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
@@ -1343,8 +1343,8 @@ class knn_cl():
             tf.local_variables_initializer().run()
             self.train_data = self.train_data_whole[i]
             self.test_data = self.test_data_whole[i]
-            print("im here in train representation")
-            self.train_representation()
+            #print("im here in train representation")
+            #self.train_representation()
             #self.construct_knn_graph_attribute()
             print("im here in train")
             self.train()
