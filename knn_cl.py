31a114ed7234c1962fd89d83d4c2da2cef1f7157
Tingyi <tiwanyan@gmail.com>
2021-02-17 05:07:57 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -47,11 +47,11 @@ class knn_cl():
         self.input_seq = []
         self.threshold = 0.5
         self.check_num_threshold_neg = 2*self.batch_size
-        self.positive_lab_size = 5
+        self.positive_lab_size = 6
         length_train = len(self.train_data)
         #iteration = np.int(np.floor(np.float(length_train) / self.batch_size))
         self.check_num_threshold_pos = 4*15#self.positive_lab_size
-        self.negative_lab_size = 5#self.batch_size-1
+        self.negative_lab_size = self.batch_size-1
         self.negative_lab_size_knn = self.negative_lab_size
         self.knn_neighbor_numbers = self.positive_lab_size
         self.positive_sample_size = self.positive_lab_size# + 1
