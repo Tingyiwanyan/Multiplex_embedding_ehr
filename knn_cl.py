b0de71843443675f9801afdef0583db85a272a0e
Tingyi <tiwanyan@gmail.com>
2021-02-26 14:54:05 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -609,7 +609,7 @@ class knn_cl():
         self.focal_loss = tf.reduce_mean(self.focal_loss_)
         self.train_step_fl = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.focal_loss)
         self.train_step_combine_fl = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(
-            self.focal_loss + 0.2 * self.negative_sum_contrast)
+            self.focal_loss + 0.6 * self.negative_sum_contrast)
         self.sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
