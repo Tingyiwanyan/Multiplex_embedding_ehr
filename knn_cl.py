0a0fd029fc6c50230684358b646414d41aaf7cf4
Tingyi <tiwanyan@gmail.com>
2021-02-27 16:18:35 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -31,7 +31,7 @@ class knn_cl():
             if self.kg.dic_patient[i]['death_flag'] == 1:
                 if i in self.train_data:
                     self.train_death_data.append(i)
-        random_pick_death = list(np.array(self.train_data)[0:2000])
+        random_pick_death = list(np.array(self.train_data)[0:3000])
         reduced_data = [i for i in self.train_data if i not in random_pick_death]
         self.train_data = reduced_data
         self.gamma = 2
