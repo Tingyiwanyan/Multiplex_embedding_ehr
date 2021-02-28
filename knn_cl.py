c27c0a96282ffd59296bc554ccf6f432a209ed88
Tingyi <tiwanyan@gmail.com>
2021-02-28 14:06:36 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -32,7 +32,8 @@ class knn_cl():
                 if i in self.train_data:
                     self.train_death_data.append(i)
         random_pick_death = list(np.array(self.train_data)[0:1500])
-        reduced_data = [i for i in self.train_data if i not in random_pick_death]
+        random_pick_non_death = list(np.array(self.train_data[0:1387]))
+        reduced_data = [i for i in self.train_data if i not in random_pick_non_death]
         self.train_data = reduced_data
         self.gamma = 2
         self.softmax_weight_threshold = 0.1
