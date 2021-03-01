e8d66c03853fe0c83868347d4f1cab0b395ae52e
Tingyi <tiwanyan@gmail.com>
2021-02-28 20:01:52 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -37,7 +37,7 @@ class knn_cl():
                     self.train_non_death_data.append(i)
         random_pick_death = list(np.array(self.train_death_data)[0:88])
         random_pick_non_death = list(np.array(self.train_non_death_data[0:2781]))
-        reduced_data = [i for i in self.train_data if i not in random_pick_non_death]
+        reduced_data = [i for i in self.train_data if i not in random_pick_death]
         self.train_data = reduced_data
         reduced_data_death = [i for i in self.train_data if i not in random_pick_non_death]
         self.train_data = reduced_data_death
