27771e2efb257b975185decc7c670e732cf89d30
Tingyi <tiwanyan@gmail.com>
2021-03-06 18:14:52 -0500

    correction

--- MLP_CL.py
+++ MLP_CL.py
@@ -33,11 +33,14 @@ class NN_model():
         self.lab_size = len(list(kg.dic_lab.keys()))
         self.input_size = self.item_size+self.demo_size+self.lab_size
         self.input_seq = []
+        self.positive_sample_size = 1
+        self.negative_sample_size = self.batch_size
         """
         define shallow neural network
         """
-        self.input_x = tf.placeholder(tf.float32, [None, self.item_size])
-        self.input_y_diag = tf.placeholder(tf.float32, [None, self.diagnosis_size])
+        self.input_x = tf.placeholder(tf.float32, [None, self.input_size])
+        self.input_x_positive = tf.placeholder(tf.float32,[None,self.positive_sample_size,self.input_size])
+        self.input_x_negative = tf.placeholder(tf.float32,[None,self.negative_sample_size,self.input_size])
 
     def softmax_loss(self):
         """
@@ -57,9 +60,45 @@ class NN_model():
         self.cross_entropy = tf.reduce_mean(tf.math.negative(
             tf.reduce_sum(tf.math.multiply(self.input_y_diag, tf.log(self.logit_softmax)), reduction_indices=[1])))
 
+    def contrastive_loss(self):
+        """
+        Implement Contrastive Loss
+        """
+        """
+        positive inner product
+        """
+        self.positive_broad = tf.broadcast_to(self.input_x,
+                                              [self.batch_size,self.positive_sample_size,self.input_size])
+        self.negative_broad = tf.broadcast_to(self.input_x,
+                                              [self.batch_size,self.negative_sample_size,self.input_size])
+
+        self.positive_broad_norm = tf.math.l2_normalize(self.positive_broad,axis=2)
+        self.positive_sample_norm = tf.math.l2_normalize(self.input_x_positive,axis=2)
+
+        self.positive_dot_prod = tf.multiply(self.positive_broad_norm,self.positive_sample_norm)
+        self.positive_dot_prod_sum = tf.reduce_sum(tf.reduce_sum(self.positive_dot_prod, 2),1)
+
+        """
+        negative inner product
+        """
+        self.negative_broad_norm = tf.math.l2_normalize(self.negative_broad,axis=2)
+        self.negative_sample_norm = tf.math.l2_normalize(self.input_x_negative,axis=2)
+
+        self.negative_dot_prod = tf.multiply(self.negative_broad_norm,self.negative_sample_norm)
+        self.negative_dot_prod_sum = tf.reduce_sum(tf.reduce_sum(self.negative_dot_prod,2),1)
+
+        """
+        Compute normalized probability and take log form
+        """
+        self.denominator_normalizer = tf.math.add(self.positive_dot_prod_sum,self.negative_dot_prod_sum)
+        self.normalized_prob = tf.math.divide(self.positive_dot_prod_sum,self.denominator_normalizer)
+        self.log_normalized_prob = tf.math.negative(tf.math.log(self.normalized_prob))
+
+
+
     def SGNN_loss_contrast(self):
         """
-        mplement sgnn loss contrast
+        implement sgnn loss contrast
         """
         negative_training_norm = tf.math.l2_normalize(self.x_negative_contrast, axis=2)
 
@@ -90,6 +129,86 @@ class NN_model():
 
         self.negative_sum_contrast = tf.math.negative(
             tf.reduce_sum(tf.math.add(sum_log_dot_prod, sum_log_dot_prod_positive)))
+
+
+    def get_batch_train(self, start_index):
+        """
+        get training batch data
+        """
+        train_one_batch_vital = np.zeros((data_length, self.item_size))
+        train_one_batch_lab = np.zeros((data_length, self.lab_size))
+        train_one_batch_demo = np.zeros((data_length, self.demo_size))
+        self.real_logit = np.zeros((data_length, 1))
+
+
+    def get_batch_train_origin(self, data_length, start_index, data):
+        """
+        get training batch data
+        """
+        train_one_batch_vital = np.zeros(
+            (data_length, self.time_sequence, 1 + self.positive_lab_size + self.negative_lab_size, self.item_size))
+        train_one_batch_lab = np.zeros(
+            (data_length, self.time_sequence, 1 + self.positive_lab_size + self.negative_lab_size, self.lab_size))
+        train_one_batch_icu_intubation = np.zeros((data_length,self.time_sequence,1+self.positive_lab_size+self.negative_lab_size,2))
+        train_one_batch_demo = np.zeros(
+            (data_length, 1 + self.positive_lab_size + self.negative_lab_size, self.demo_size))
+        train_one_batch_com = np.zeros(
+            (data_length, 1 + self.positive_lab_size + self.negative_lab_size, self.com_size))
+        # train_one_batch_item = np.zeros((data_length,self.positive_lab_size+self.negative_lab_size,self.item_size))
+        train_one_batch_mortality = np.zeros((data_length, 2, 2))
+        one_batch_logit = np.zeros((data_length, 2))
+        self.neg_patient_id = []
+        for i in range(data_length):
+            self.patient_id = data[start_index + i]
+            self.neg_patient_id.append(self.patient_id)
+        for i in range(data_length):
+            self.patient_id = data[start_index + i]
+            # if self.kg.dic_patient[self.patient_id]['item_id'].keys() == {}:
+            #   index_increase += 1
+            #  continue
+            # index_batch += 1
+            self.time_seq = self.kg.dic_patient[self.patient_id]['prior_time_vital'].keys()
+            self.time_seq_int = [np.int(k) for k in self.time_seq]
+            self.time_seq_int.sort()
+            time_index = 0
+            flag = self.kg.dic_patient[self.patient_id]['death_flag']
+            """
+            if flag == 0:
+                one_batch_logit[i,0,0] = 1
+                one_batch_logit[i,1,1] = 1
+            else:
+                one_batch_logit[i,0,1] = 1
+                one_batch_logit[i,1,0] = 1
+                self.real_logit[i] = 1
+            """
+            if flag == 0:
+                train_one_batch_mortality[i, 0, :] = [1, 0]
+                train_one_batch_mortality[i, 1, :] = [0, 1]
+                one_batch_logit[i, 0] = 1
+                self.real_logit[i,0] = 0
+            else:
+                train_one_batch_mortality[i, 0, :] = [0, 1]
+                train_one_batch_mortality[i, 1, :] = [1, 0]
+                one_batch_logit[i, 1] = 1
+                self.real_logit[i,0] = 1
+
+            self.get_positive_patient(self.patient_id)
+            self.get_negative_patient(self.patient_id)
+            train_one_data_vital = np.concatenate((self.patient_pos_sample_vital, self.patient_neg_sample_vital),
+                                                  axis=1)
+            train_one_data_lab = np.concatenate((self.patient_pos_sample_lab, self.patient_neg_sample_lab), axis=1)
+            train_one_data_demo = np.concatenate((self.patient_pos_sample_demo, self.patient_neg_sample_demo), axis=0)
+            train_one_data_com = np.concatenate((self.patient_pos_sample_com, self.patient_neg_sample_com), axis=0)
+            train_one_data_icu_intubation = np.concatenate((self.patient_pos_sample_icu_intubation_label,self.patient_neg_sample_icu_intubation_label),axis=1)
+            train_one_batch_vital[i, :, :, :] = train_one_data_vital
+            train_one_batch_lab[i, :, :, :] = train_one_data_lab
+            train_one_batch_demo[i, :, :] = train_one_data_demo
+            train_one_batch_com[i, :, :] = train_one_data_com
+            train_one_batch_icu_intubation[i,:,:,:] = train_one_data_icu_intubation
+
+
+
+        return train_one_batch_vital, train_one_batch_lab, train_one_batch_demo, one_batch_logit, train_one_batch_mortality, train_one_batch_com,train_one_batch_icu_intubation
 
     def assign_value_vital(self, patientid):
         self.one_sample = np.zeros(self.item_size)
@@ -148,10 +267,7 @@ class NN_model():
     def assign_value_lab(self, patientid, start_time, end_time):
         self.one_sample_lab = np.zeros(self.lab_size)
         self.times_lab = []
-        for i in self.kg.dic_patient[patientid]['prior_time_lab'].keys():
-            if (np.int(i) > start_time or np.int(i) == start_time) and np.int(i) < end_time:
-                self.times_lab.append(i)
-        for j in self.times_lab:
+        for j in self.kg.dic_patient[patientid]['prior_time_lab'].keys():
             for i in self.kg.dic_patient[patientid]['prior_time_lab'][str(j)].keys():
                 if i[-1] == 'A':
                     continue
@@ -165,21 +281,27 @@ class NN_model():
                     continue
                 mean = np.float(self.kg.dic_lab[i]['mean_value'])
                 std = np.float(self.kg.dic_lab[i]['std'])
-                in_lier = np.where(np.array(self.kg.dic_patient[patientid]['prior_time_lab'][str(j)][i])<mean+3*std)[0]
-                in_lier_value = list(np.array(self.kg.dic_patient[patientid]['prior_time_lab'][str(j)][i])[in_lier])
-                if in_lier_value == []:
-                    ave_value = mean
-                else:
-                    #ave_value = np.mean([np.float(k) for k in self.kg.dic_patient[patientid]['prior_time_lab'][str(j)][i]])
-                    ave_value = np.mean(
-                        [np.float(k) for k in in_lier_value])
+                #in_lier = np.where(np.array(self.kg.dic_patient[patientid]['prior_time_lab'][str(j)][i])<mean+3*std)[0]
+                #in_lier_value = list(np.array(self.kg.dic_patient[patientid]['prior_time_lab'][str(j)][i]))#[in_lier])
+                #if in_lier_value == []:
+                    #ave_value = mean
+                #else:
+                ave_value = np.mean([np.float(k) for k in self.kg.dic_patient[patientid]['prior_time_lab'][str(j)][i]])
+                    #ave_value = np.mean(
+                        #[np.float(k) for k in in_lier_value])
                 index = self.kg.dic_lab[i]['index']
                 #self.ave_lab[index] = mean
                 if std == 0:
                     self.one_sample_lab[index] += 0
                     #self.freq_sample_lab[index] += 1
                 else:
-                    self.one_sample_lab[index] += np.float(ave_value) - mean / 3*std
+                    if ave_value > mean+std:
+                        self.one_sample[index] = 1
+                    elif ave_value < mean-std:
+                        self.one_sample[index] = -1
+                    else:
+                        self.one_sample[index] = (np.float(ave_value)-mean)/std
+                    #self.one_sample_lab[index] += (np.float(ave_value) - mean) / std
                     self.freq_sample_lab[index] += 1
 
         out_sample_lab = self.one_sample_lab / self.freq_sample_lab
