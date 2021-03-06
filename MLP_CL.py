50baa8477b40db80996a4a8f5b82963e6019856b
Tingyi <tiwanyan@gmail.com>
2021-03-06 18:18:21 -0500

    correction

--- MLP_CL.py
+++ MLP_CL.py
@@ -12,10 +12,10 @@ class NN_model():
     """
     Create shalow neural network model for EHR data
     """
-    def __init__(self,kg,hetro_model,data_process):
+    def __init__(self,kg,data_process):
         self.kg = kg
         self.data_process = data_process
-        self.hetro_model = hetro_model
+        #self.hetro_model = hetro_model
         self.train_data = self.data_process.train_patient
         self.test_data = self.data_process.test_patient
         self.length_train = len(self.train_data)
