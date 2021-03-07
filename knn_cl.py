75f1419e047f6eaed54ebf79c7c4204ca2512c81
Tingyi <tiwanyan@gmail.com>
2021-03-07 03:57:05 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -60,12 +60,12 @@ class knn_cl():
         for i in kg.dic_lab.keys():
             index = self.kg.dic_lab[i]['index']
             self.ave_lab[index] = self.kg.dic_lab[i]['mean_value']
-        random_pick_death = list(np.array(self.train_data)[0:3000])
+        random_pick_death = list(np.array(self.train_data)[0:1000])
         random_pick_non_death = list(np.array(self.train_non_death_data[0:2810]))
         random_pick_death_test = list(np.array(self.test_death_data[0:300]))
         reduced_data = [i for i in self.train_data if i not in random_pick_death]
         reduced_data_test = [i for i in self.test_data if i not in random_pick_death_test]
-        #self.train_data = reduced_data
+        self.train_data = reduced_data
         #self.test_data = reduced_data_test
         for i in self.kg.dic_patient.keys():
             if self.kg.dic_patient[i]['death_flag'] == 1:
@@ -93,7 +93,7 @@ class knn_cl():
         self.latent_dim_cell_state = 100
         self.latent_dim_att = 100
         self.latent_dim_demo = 50
-        self.epoch = 5
+        self.epoch = 4
         self.epoch_representation = 1
         self.latent_dim = self.item_size + self.lab_size
         self.com_size = 12
@@ -609,6 +609,7 @@ class knn_cl():
         self.cross_entropy = bce(self.logit_sig, self.input_y_logit)
         self.train_step_ce = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.cross_entropy)
         self.train_step_combine_ce = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.cross_entropy+0.2*self.log_normalized_prob)
+        self.train_step_cl = self.log_normalized_prob
         """
         focal loss
         """
@@ -1459,7 +1460,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train_origin(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_fl],
+                self.err_ = self.sess.run([self.cross_entropy, self.train_step_cl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
@@ -1504,7 +1505,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_fl],
+                self.err_ = self.sess.run([self.cross_entropy, self.train_step_cl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
