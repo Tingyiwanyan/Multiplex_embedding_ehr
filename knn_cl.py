bf04fa171b256a603293ad155f0cc36536ffddde
Tingyi <tiwanyan@gmail.com>
2021-03-07 03:15:16 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -65,8 +65,8 @@ class knn_cl():
         random_pick_death_test = list(np.array(self.test_death_data[0:300]))
         reduced_data = [i for i in self.train_data if i not in random_pick_death]
         reduced_data_test = [i for i in self.test_data if i not in random_pick_death_test]
-        self.train_data = reduced_data
-        self.test_data = reduced_data_test
+        #self.train_data = reduced_data
+        #self.test_data = reduced_data_test
         for i in self.kg.dic_patient.keys():
             if self.kg.dic_patient[i]['death_flag'] == 1:
                 if i in self.train_data:
@@ -93,14 +93,14 @@ class knn_cl():
         self.latent_dim_cell_state = 100
         self.latent_dim_att = 100
         self.latent_dim_demo = 50
-        self.epoch = 4
+        self.epoch = 5
         self.epoch_representation = 1
         self.latent_dim = self.item_size + self.lab_size
         self.com_size = 12
         self.input_seq = []
         self.threshold = 0.5
         self.check_num_threshold_neg = 2*self.batch_size
-        self.positive_lab_size = 15
+        self.positive_lab_size = 5
         length_train = len(self.train_data)
         #iteration = np.int(np.floor(np.float(length_train) / self.batch_size))
         self.check_num_threshold_pos = 4*15#self.positive_lab_size
