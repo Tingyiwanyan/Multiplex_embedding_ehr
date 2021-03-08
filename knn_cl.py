ce2443f07147e7f95b3d84a30b5c2508f0ff47f7
Tingyi <tiwanyan@gmail.com>
2021-03-08 18:49:02 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -60,12 +60,12 @@ class knn_cl():
         for i in kg.dic_lab.keys():
             index = self.kg.dic_lab[i]['index']
             self.ave_lab[index] = self.kg.dic_lab[i]['mean_value']
-        random_pick_death = list(np.array(self.train_death_data)[0:500])
+        random_pick_death = list(np.array(self.train_data)[0:1000])
         random_pick_non_death = list(np.array(self.train_non_death_data[0:2810]))
         random_pick_death_test = list(np.array(self.test_death_data[0:300]))
         reduced_data = [i for i in self.train_data if i not in random_pick_death]
         reduced_data_test = [i for i in self.test_data if i not in random_pick_death_test]
-        #self.train_data = reduced_data
+        self.train_data = reduced_data
         #self.test_data = reduced_data_test
         for i in self.kg.dic_patient.keys():
             if self.kg.dic_patient[i]['death_flag'] == 1:
@@ -1485,7 +1485,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train_origin(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_ce],
+                self.err_ = self.sess.run([self.cross_entropy, self.train_step_cl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
@@ -1530,7 +1530,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_ce],
+                self.err_ = self.sess.run([self.cross_entropy, self.train_step_cl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
