44c74e2fdd41d11a3ca32b178dbf2763471abdfe
Tingyi <tiwanyan@gmail.com>
2021-03-13 23:47:40 -0500

    correction

--- knn_cl.py
+++ knn_cl.py
@@ -1568,7 +1568,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train_origin(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_fl],
+                self.err_ = self.sess.run([self.cross_entropy, self.train_step_cl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
@@ -1613,7 +1613,7 @@ class knn_cl():
                 self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com,self.one_batch_icu_intubation = self.get_batch_train(
                     self.batch_size, i * self.batch_size, self.train_data)
 
-                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_fl],
+                self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_cl],
                                           feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                      self.input_x_lab: self.train_one_batch_lab,
                                                      self.input_x_demo: self.train_one_batch_demo,
@@ -1646,7 +1646,7 @@ class knn_cl():
                     self.train_one_batch_vital, self.train_one_batch_lab, self.train_one_batch_demo, self.one_batch_logit, self.one_batch_mortality, self.one_batch_com, self.one_batch_icu_intubation = self.get_batch_train_origin(
                         self.batch_size, i * self.batch_size, self.train_data)
 
-                    self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_fl],
+                    self.err_ = self.sess.run([self.cross_entropy, self.train_step_combine_cl],
                                               feed_dict={self.input_x_vital: self.train_one_batch_vital,
                                                          self.input_x_lab: self.train_one_batch_lab,
                                                          self.input_x_demo: self.train_one_batch_demo,
