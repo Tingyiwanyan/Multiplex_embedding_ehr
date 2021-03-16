77a964ee5b507337658b84bc3b0e1b721cbbe27e
Tingyi <tiwanyan@gmail.com>
2021-03-16 08:49:44 -0400

    correction

--- seaborn_plot.py
+++ seaborn_plot.py
@@ -2,6 +2,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
+import pandas as pd
+import umap.umap_ as umap
 
 
 CL_k_att = np.load('embedding_whole_att.npy')
@@ -12,6 +14,10 @@ CL_k_att_logit = np.load('embedding_whole_att_logit.npy')
 
 CL_k = np.load('embedding_whole_random.npy')
 CL_k_logit = np.load('embedding_whole_random_logit.npy')
+
+reducer = umap.UMAP()
+CL_k = reducer.fit_transform(CL_k)
+CL_k_att = reducer.fit_transform(CL_k_att)
 
 kmeans_train_death_att = KMeans(n_clusters=1, random_state=0).fit(CL_k_att[np.where(CL_k_att_logit==1)[0],:])
 
@@ -30,7 +36,12 @@ kmeans_train_live = KMeans(n_clusters=1, random_state=0).fit(CL_k[np.where(CL_k_
 train_live_center = kmeans_train_live.cluster_centers_
 
 
+df = pd.DataFrame({"axes1":CL_k[:,0], "axes2":CL_k[:,1], "Mortality Label":CL_k_logit[:,0]})
 
-#geyser = sns.load_dataset("geyser")
-#sns.kdeplot(data=geyser, x="waiting", y="duration",hue="kind",fill=True,)
-#plt.show()
+#sns.kdeplot(data=df, x="waiting", y="duration",hue="kind",)#fill=True,)
+sns.jointplot(data=df, x='axes1', y='axes2',hue="Mortality Label",marker='o').plot_joint(sns.kdeplot,levels=7)
+plt.show()
+
+df_att = pd.DataFrame({"axes1":CL_k_att[:,0], "axes2":CL_k_att[:,1], "Mortality Label":CL_k_att_logit[:,0]})
+sns.jointplot(data=df_att, x="axes1", y="axes2",hue="Mortality Label",marker='o').plot_joint(sns.kdeplot,levels=7)
+plt.show()
