3bb361e44ec340f6c0ab7a75c334005f9b876a93
Tingyi <tiwanyan@gmail.com>
2021-05-13 13:59:05 -0400

    correction

--- kg_constraction_whole_new.py
+++ kg_constraction_whole_new.py
@@ -749,7 +749,7 @@ if __name__ == "__main__":
     reduced_data_intubate = [i for i in kg.total_data_intubation if i not in random_pick_intubate]
     kg.total_data_intubation = reduced_data_intubate
 
-    random_pick_icu = random.sample(icu_data, 850)
+    random_pick_icu = random.sample(icu_data, 960)
     reduced_data_icu = [i for i in kg.total_data_icu if i not in random_pick_icu]
     kg.total_data_icu = reduced_data_icu
 
